acb87cadbde876d0a806e530cf8b616671094c9b
Alvaro Garcia <derecerca@gmail.com>
2017-07-26 01:13:48 +0200

    Refactor: extract explaining functions (to a letfn)

--- src/greetings_kata/core.clj
+++ src/greetings_kata/core.clj
@@ -7,9 +7,10 @@
   [name]
   (letfn [(upper-case? [line] (and (> (count line) 0) (every? #(Character/isUpperCase %) line)))
           (anonymous? [name] (nil? name))
-          (anonymous-greeting [_] "Hello, my friend.")]
+          (anonymous-greeting [_] "Hello, my friend.")
+          (personalized-greeting [name] (str "Hello, " name "."))]
     (let [
-          greeting (if (anonymous? name) anonymous-greeting (fn [name] (str "Hello, " name ".")))
+          greeting (if (anonymous? name) anonymous-greeting personalized-greeting)
           greeting (if (upper-case? name) (comp str/upper-case greeting) greeting)]
       (greeting name))))
 
