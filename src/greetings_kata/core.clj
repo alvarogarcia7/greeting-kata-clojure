70e4c63f93c63f9bf7469df9163c4316d7a24f89
Alvaro Garcia <derecerca@gmail.com>
2017-07-26 01:05:46 +0200

    Refactor: extract explaining functions (to a letfn)

--- src/greetings_kata/core.clj
+++ src/greetings_kata/core.clj
@@ -4,10 +4,11 @@
 (defn
   greet
   [name]
-  (let [greeting "Hello, "
-        value (if (nil? name) "my friend" name)
-        upper-case? (fn [line] (and (> (count line) 0) (every? #(Character/isUpperCase %) line)))
-        greeting (if (upper-case? name) (clojure.string/upper-case greeting) greeting)]
-    (str greeting value ".")))
+  (letfn [(upper-case? [line] (and (> (count line) 0) (every? #(Character/isUpperCase %) line)))
+          (anonymous? [name] (nil? name))]
+    (let [greeting "Hello, "
+          value (if (anonymous? name) "my friend" name)
+          greeting (if (upper-case? name) (clojure.string/upper-case greeting) greeting)]
+      (str greeting value "."))))
 
 
