14dc6850b3f9b1d2e3343894ac1d8080ff59eceb
Alvaro Garcia <derecerca@gmail.com>
2017-07-25 22:40:16 +0200

    Add project with dependencies

---
+++ test/greetings_kata/core_test.clj
@@ -0,0 +1,7 @@
+(ns greetings-kata.core-test
+  (:require [clojure.test :refer :all]
+            [greetings-kata.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
