9dd2b12effbcd8951cf371a713f85c7eba2ccec2
Alvaro Garcia <derecerca@gmail.com>
2017-07-26 02:11:49 +0200

    Refactor: convert a method to a multi-method

--- src/greetings_kata/core.clj
+++ src/greetings_kata/core.clj
@@ -3,16 +3,24 @@
   (:require [clojure.string :as str]))
 
 (defn
+  upper-case_?
+  [line]
+  (and (> (count line) 0) (every? #(Character/isUpperCase %) line)))
+
+(defmulti upper-case? (fn [name] (coll? name)) :default name)
+(defmethod upper-case? true [names] (every? upper-case_? names))
+(defmethod upper-case? false [name] (upper-case_? name))
+
+(defn
   greet
   [name]
-  (letfn [(upper-case? [line] (and (> (count line) 0) (every? #(Character/isUpperCase %) line)))
-          (anonymous? [name] (nil? name))
+  (letfn [(anonymous? [name] (nil? name))
           (base-greeting [rest] (str "Hello, " rest "."))
           (anonymous-greeting [_] (base-greeting "my friend"))
           (personalized-greeting [name] (base-greeting name))]
     (let [
           greeting (if (anonymous? name) anonymous-greeting personalized-greeting)
-          greeting (if (if (coll? name) (every? upper-case? name) (upper-case? name)) (comp str/upper-case greeting) greeting)
+          greeting (if (upper-case? name) (comp str/upper-case greeting) greeting)
           names (if (coll? name) (str/join " and " name) name)
           ]
       (greeting names))))
