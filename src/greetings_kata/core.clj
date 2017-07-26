f4bb64f7132b23cec8e43434a8081304518eb2ac
Alvaro Garcia <derecerca@gmail.com>
2017-07-26 02:14:49 +0200

    Refactor: convert a method to a multi-method

--- src/greetings_kata/core.clj
+++ src/greetings_kata/core.clj
@@ -11,6 +11,10 @@
 (defmethod upper-case? true [names] (every? upper-case_? names))
 (defmethod upper-case? false [name] (upper-case_? name))
 
+(defmulti addressing (fn [name] (coll? name)) :default name)
+(defmethod addressing true [names] (str/join " and " names))
+(defmethod addressing false [name] name)
+
 (defn
   greet
   [name]
@@ -21,7 +25,7 @@
     (let [
           greeting (if (anonymous? name) anonymous-greeting personalized-greeting)
           greeting (if (upper-case? name) (comp str/upper-case greeting) greeting)
-          names (if (coll? name) (str/join " and " name) name)
+          names (addressing name)
           ]
       (greeting names))))
 
